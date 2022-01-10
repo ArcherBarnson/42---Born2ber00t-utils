dcb4df17e35c39775a4d6a8268dcbc676166b567
Benjamin GRULOIS <bgrulois@e2r12p7.clusters.42paris.fr>
2022-01-10 15:22:41 +0100

    monitoring.sh done

--- monitoring.sh
+++ monitoring.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
-LVM=$(vgchange -ay LVMGroup | awk 'NR == 1{printf("%s"), $9}')
+LVM=$(vgchange -ay | grep -o active)
 IPV4=$(hostname -I)
-MAC=$(cat /sys/class/net/enp3s0f0/address)
-
+MAC=$(cat /sys/class/net/enp0s3/address)
+wall \
 printf "#Architecture : " && uname -a && \
-free --mega | awk 'NR == 2{printf("#Memory Usage: %d/%dMB (%d%)"), $3, $2, $3/$2*100}' && \
-df -h /dev/sda | awk 'NR == 2{printf("\n#Disk Usage : %d/%dGb (%d%)"), $3, $2, $5}' && \
-if [$LVM == 'active']; then
-	printf "#LVM use: yes"
+printf "#CPU Physical : " && lscpu | awk 'NR == 5{printf("$2")}' && \
+printf "\nvCPU : " && lscpu | awk 'NR == 5{printf("$2")}' && \
+free --mega | awk 'NR == 2{printf("#Memory Usage: %d/%dMB (%d%%)"), $3, $2, $3/$2*100}' && \
+df -h /dev/sda5 | awk 'NR == 2{printf("\n#Disk Usage : %d/%dGb (%d%%)"), $3, $2, $5}' && \
+printf "\n#CPU Load : " && \
+grep 'cpu ' /proc/stat | awk '{printf("%d%%"), ($2+$3)*100/($2+$3+$4)}' && \
+printf "\n#Last Boot: $(who -b | awk 'NR == 1{printf("%s at %s"), $3, $4}')"
+if [ "$LVM" = "active" ]; then
+	printf "\n#LVM use: yes"
 else
-	printf "#LVM use: no"
+	printf "\n#LVM use: no"
 fi && \
-printf "\n#Network: IP $IPV4 ($MAC)"
+printf "\n#TCP Connexions : $(netstat -tan | grep -o ESTABLISHED | wc -l) ESTABLISHED" && \
+printf "\n#Users logged in : $(who | wc -l)" && \
+printf "\n#Network: IP $IPV4 ($MAC)\n" && \
+printf "#Sudo : $(tail /var/log/auth.log | wc -l) cmd\n"
